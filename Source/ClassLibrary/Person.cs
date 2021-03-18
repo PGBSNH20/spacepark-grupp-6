42ba22d7a73435c440e29c47116112b097a71e5f
Liam Björkman <liambjorkman00@outlook.com>
2021-03-18 10:47:28 +0100

    Testing

--- Source/ClassLibrary/Person.cs
+++ Source/ClassLibrary/Person.cs
@@ -8,14 +8,31 @@ namespace ClassLibrary
     public class Person
     {
         public string Count { get; set; }
-        public string Next { get; set; }
         public List<Results> Results { get; set; }
 
         public static async Task<List<Results>> GetPerson()
         {
+            var list = new List<string>();
+            string id;
             var client = new RestClient("https://swapi.dev/api/");
             var request = new RestRequest("people/", DataFormat.Json);
             var response = await client.GetAsync<Person>(request);
+
+            //for (int i = 0; i < response.Results.Count; i++)
+            //{
+            //    for (int j = 0; j < response.Results[i].Starships.Count; j++)
+            //    {
+            //        var s = response.Results[i].Starships[j].Split("/");
+            //        id = s[5];
+            //        var t = await Starship.GetStarships(id);
+            //        foreach (var ship in t)
+            //        {
+            //            response.Results[i].Ships.Add(ship);
+            //        }
+            //    }
+
+            //}
+
             return response.Results;
         }
     }
@@ -23,13 +40,20 @@ namespace ClassLibrary
     {
         public string Name { get; set; }
         public string Height { get; set; }
-
-
     }
     public class Starship
     {
         public string Name { get; set; }
         public string Length { get; set; }
+        public List<Starship> Starships {get; set;}
+
+        public static async Task<List<Starship>> GetStarships()
+        {
+            var client = new RestClient("https://swapi.dev/api/");
+            var request = new RestRequest("starships/", DataFormat.Json);
+            var response = await client.GetAsync<Starship>(request);
+            return response;
+        }
     }
 
 }
--- Source/SpacePark/Program.cs
+++ Source/SpacePark/Program.cs
@@ -10,11 +10,25 @@ namespace SpacePark
     {
         static void Main(string[] args)
         {
+        //    var t = Starship.GetStarships("22");
+        //    Console.WriteLine(t.Result);
             Console.WriteLine("What is your name?");
             string name = Console.ReadLine();
             var r = Person.GetPerson();
+            var ship = Starship.GetStarships();
 
-            Console.WriteLine(r.Result.Any(p => p.Name == name) ? "Welcome" : "Not Allowed");
+            Console.WriteLine(r.Result.Count);
+
+            if (r.Result.Any(p => p.Name == name))
+            {
+                Console.WriteLine($"Welcome {name}. What ship will you be parking today?");
+                //ListShips();
+            }
+            else
+            {
+                Console.WriteLine("Not Allowed");
+            }
+
         }
     }
 }
